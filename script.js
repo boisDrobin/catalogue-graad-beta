12d1c6be4e11bcb16a864fab013706df672f0d07
boisDrobin <150900236+boisDrobin@users.noreply.github.com>
2026-05-06 16:07:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let activePublicFamily = "medecins";
 let activeSpecialty = "";
 
 const CSV_PATH = "./data/data.csv";
-const CSV_IMPORT_DATE = "2026-05-04";
+const CSV_IMPORT_DATE = "2026-05-06";
 
 const multiFilterState = {
   format: [],
